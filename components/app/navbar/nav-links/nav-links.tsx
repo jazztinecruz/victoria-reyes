cde275efbe60f4b7c17fc0618056e9f51758b2fa
jazztine <jzztn.crz@gmail.com>
2022-11-04 23:35:26 +0800

    Make it hidden in notebook

--- components/app/navbar/nav-links/nav-links.tsx
+++ components/app/navbar/nav-links/nav-links.tsx
@@ -2,13 +2,15 @@ import NavLink from "../nav-link/nav-link";
 
 const NavLinks = () => {
   return (
-    <div className="flex items-center gap-5">
-      {/* navlinks */}
-      <NavLink name="Home" link="#" />
-      <NavLink name="About Us" link="#" />
-      <NavLink name="Barangay Documents" link="#" />
-      <NavLink name="What's New?" link="#" />
-      <NavLink name="Contact Us" link="#" />
+    <div className="notebook:hidden laptop:block ">
+      <div className="flex items-center gap-5">
+        {/* navlinks */}
+        <NavLink name="Home" link="#" />
+        <NavLink name="About Us" link="#" />
+        <NavLink name="Barangay Documents" link="#" />
+        <NavLink name="What's New?" link="#" />
+        <NavLink name="Contact Us" link="#" />
+      </div>
     </div>
   );
 };
