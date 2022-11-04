faa4854ba7ddfb8580974431f80fdb321d7b1680
ntabucejo <ntabucejo@gmail.com>
2022-11-04 15:56:11 +0800

    Test database on the pages directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
+import { GetServerSideProps } from "next";
+import database from "../library/database";
+
+const HomePage = ({ users }: any) => {
+  console.log(users);
   return <div>Home Page Here...</div>;
 };
 
 export default HomePage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const users = await database.user.findMany();
+  return {
+    props: {
+      users,
+    },
+  };
+};
