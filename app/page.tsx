8dfb58d712eac0d85171adaab424a7a61a2c1995
jazztine <jzztn.crz@gmail.com>
2022-11-05 00:35:32 +0800

    Modify hero text content styles

--- app/page.tsx
+++ app/page.tsx
@@ -23,19 +23,19 @@ const HomePage = () => {
             <h1 className="text-3xl font-semibold tracking-wider text-white laptop:text-6xl">
               BARANGAY VICTORIA REYES
             </h1>
-            <h3 className="text-lg laptop:text-2xl font-light tracking-wider text-white">
+            <h3 className="text-lg laptop:text-3xl font-light tracking-wider text-white">
               CITY OF DASMARINAS
             </h3>
 
             {/* sign up */}
             <div className="mt-8 grid w-full grid-flow-row items-center laptop:grid-cols-[1fr,auto]">
-              <div className="rounded-tl-md rounded-tr-md bg-white py-6 px-12 laptop:rounded-bl-md laptop:rounded-tr-none">
+              <div className="rounded-tl-md rounded-tr-md bg-white py-4 laptop:py-6 px-12 laptop:rounded-bl-md laptop:rounded-tr-none">
                 <span className="cursor-pointer text-center text-[#838383]">
                   Dont have an account yet?
                 </span>
               </div>
 
-              <button className="flex h-full items-center justify-center gap-5 rounded-br-md rounded-bl-md bg-green py-6 px-14 text-center text-white laptop:justify-start laptop:rounded-tr-md laptop:rounded-bl-none laptop:py-0 laptop:text-start">
+              <button className="flex h-full items-center justify-center gap-5 rounded-br-md rounded-bl-md bg-green  py-4 px-14 text-center text-white laptop:justify-start laptop:rounded-tr-md laptop:rounded-bl-none laptop:py-0 laptop:text-left">
                 <span className="text-lg text-white">GET STARTED</span>
                 <ChevronRightIcon className="h-6 w-6 text-white" />
               </button>
