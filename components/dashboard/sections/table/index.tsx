f04ac8ff3c3b584fc9e8f681fc7316335538fe4b
ntabucejo <ntabucejo@gmail.com>
2022-11-05 03:04:03 +0800

    Update table max-h

--- components/dashboard/sections/table/index.tsx
+++ components/dashboard/sections/table/index.tsx
@@ -13,7 +13,7 @@ const TableComponent = ({ children, name }: Props) => {
   return (
     <section className="space-content">
       <h2 className="font-medium">{name}</h2>
-      <div className="scrollbar-thumb-rounded-full max-h-[600px] overflow-x-auto scrollbar-thin  scrollbar-thumb-slate-900 scrollbar-track-slate-50">
+      <div className="scrollbar-thumb-rounded-full max-h-[60vh] overflow-x-auto scrollbar-thin  scrollbar-track-slate-50 scrollbar-thumb-slate-900">
         <table className="text-left text-sm">{children}</table>
       </div>
     </section>
