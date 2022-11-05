4fa44cc78df8f954b9e4fc42708b50f7e1361687
jazztine <jzztn.crz@gmail.com>
2022-11-05 19:00:26 +0800

    Modify tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
 import Navbar from "../components/app/navbar/navbar";
+import Documents from "./homepage-sections/documents/documents";
 
 const HomePage = () => {
   return (
@@ -12,28 +13,28 @@ const HomePage = () => {
         </nav>
 
         {/* hero content */}
-        <div className="relative grid h-full items-center justify-center bg-hero-bg bg-cover bg-no-repeat">
+        <div className="relative grid h-screen items-center justify-center bg-hero-bg bg-cover bg-no-repeat">
           {/* hero black container */}
           <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/75"></div>
 
           {/* hero content */}
-          <div className="z-50 flex flex-col items-center justify-center gap-2 text-center px-4">
-            <h1 className="text-3xl font-semibold tracking-wider text-white laptop:text-6xl">
+          <div className="z-50 flex flex-col items-center justify-center gap-2 px-4 text-center">
+            <h1 className="text-3xl font-semibold tracking-wider text-white tablet:text-5xl laptop:text-6xl">
               BARANGAY VICTORIA REYES
             </h1>
-            <h3 className="text-lg font-light tracking-wider text-white laptop:text-3xl">
+            <h3 className="text-lg font-light tracking-wider text-white tablet:text-2xl laptop:text-3xl">
               CITY OF DASMARINAS
             </h3>
 
             {/* sign up */}
-            <div className="mt-8 grid grid-flow-row justify-center w-full laptop:grid-cols-[1fr,auto]">
-              <div className="w-96 laptop:w-full py-5 pl-6 text-center laptop:text-left rounded-tl-md rounded-tr-md bg-white laptop:rounded-bl-md laptop:rounded-tr-none">
-                <span className="cursor-pointertext-[#838383]">
+            <div className="mt-8 grid w-full grid-flow-row justify-center tablet:grid-cols-[1fr,auto]">
+              <div className="w-96 rounded-tl-md rounded-tr-md bg-white py-5 pl-6 text-center tablet:w-full tablet:rounded-bl-md tablet:rounded-tr-none laptop:text-left">
+                <span className="cursor-pointer text-[#838383]">
                   Dont have an account yet?
                 </span>
               </div>
 
-              <button className="w-full laptop:w-60 grid grid-flow-col laptop:grid-cols-[1fr,auto] py-5  px-6 items-center justify-center gap-10 laptop:gap-0 rounded-br-md rounded-bl-md bg-green  text-center text-white laptop:justify-start laptop:rounded-tr-md laptop:rounded-bl-none laptop:py-0 laptop:text-left">
+              <button className="grid w-full grid-flow-col items-center justify-center gap-10  rounded-br-md rounded-bl-md bg-green py-5 px-6 text-center text-white laptop:w-60  laptop:grid-cols-[1fr,auto] laptop:justify-start laptop:gap-0 tablet:rounded-tr-md tablet:rounded-bl-none laptop:py-0 laptop:text-left">
                 <span className="text-lg text-white ">GET STARTED</span>
                 <ChevronRightIcon className="h-6 w-6 text-white" />
               </button>
@@ -42,10 +43,26 @@ const HomePage = () => {
         </div>
       </section>
 
+      {/* about */}
+      <div className="laptop:h-90 z-50 flex w-full flex-col gap-10 rounded-md px-6 py-20 laptop:py-16 text-center text-lg leading-relaxed laptop:absolute laptop:-bottom-52 laptop:left-0 laptop:right-0 laptop:w-[945px] laptop:translate-x-2/4 laptop:transform laptop:bg-white laptop:shadow-md">
+        <span className="text-xl font-semibold laptop:hidden">About Us</span>
+        <p>
+          Barangay Victoria Management System is a website that entails to
+          provide a user-friendly and convenient interface in terms of
+          requesting and processing registration within its community. This
+          serves as an automated service so that the user may have an option to
+          process and request for the services without the need to walk-in on
+          barangay centers.
+        </p>
+      </div>
+
+      {/* barangay documents */}
+      <Documents />
+
       {/* footer */}
-      <footer>
+      {/* <footer>
         <div className="mx-auto max-w-5xl border-2">footer</div>
-      </footer>
+      </footer> */}
     </div>
   );
 };
