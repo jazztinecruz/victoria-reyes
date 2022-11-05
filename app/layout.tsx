bbaeae0635ca6408f3e998b36171ff6ea00e6cc5
ntabucejo <ntabucejo@gmail.com>
2022-11-05 14:04:28 +0800

    Add meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,9 @@ const poppins = Poppins({
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={poppins.className}>
-      <head></head>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className="bg-slate-50 text-slate-900">{children}</body>
     </html>
   );
