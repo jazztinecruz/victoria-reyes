39b1c42f7a59c19ae51321ff780a4ab0272dbc64
jazztine <jzztn.crz@gmail.com>
2022-11-08 23:31:48 +0800

    Change color green to brand

--- components/elements/button/button.tsx
+++ components/elements/button/button.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 const Button = ({ name }: Props) => {
   return (
-    <button className="cursor-pointer rounded-md bg-green py-3 px-6 text-sm text-white transition-all duration-500 hover:scale-105 hover:bg-green/90">
+    <button className="cursor-pointer rounded-md bg-brand py-3 px-6 text-sm text-white transition-all duration-500 hover:scale-105 hover:bg-brand/90">
       {name}
     </button>
   );
