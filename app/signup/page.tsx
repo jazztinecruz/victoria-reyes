47deb7ef1d5fd4f7791069ffb4ae93af36c2ec15
jazztine <jzztn.crz@gmail.com>
2022-11-11 18:17:20 +0800

    Include Create my account button in form

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -167,15 +167,17 @@ const SignUp = () => {
                   onChange={setFields}
                 />
               </div>
+
+              <div></div>
+
+              <button
+                onClick={() => setSuccessfulModal(!sucessfulModal)}
+                className="mt-10 flex w-full transform items-center justify-between rounded-md bg-brand px-6 py-5 text-sm capitalize tracking-wide text-white transition-colors duration-300 hover:bg-brand hover:opacity-fade focus:outline-none focus:ring focus:ring-brand focus:ring-opacity-50">
+                <span className="text-md">Create my Account</span>
+
+                <ChevronRightIcon className="h-5 w-5 text-white" />
+              </button>
             </form>
-
-            <button
-              onClick={() => setSuccessfulModal(!sucessfulModal)}
-              className="rounded-md mt-10 flex w-full transform items-center justify-between bg-brand px-6 py-5 text-sm capitalize tracking-wide text-white transition-colors duration-300 hover:bg-brand hover:opacity-fade focus:outline-none focus:ring focus:ring-brand focus:ring-opacity-50">
-              <span className="text-md">Create my Account</span>
-
-              <ChevronRightIcon className="h-5 w-5 text-white" />
-            </button>
 
             {sucessfulModal && (
               <Modal
