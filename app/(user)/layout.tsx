bd510b3b2262daee392d2e76dc30a7dd82a74f8f
jazztine <jzztn.crz@gmail.com>
2022-11-16 23:13:13 +0800

    Add notification and logout

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,15 +1,20 @@
-import { BellIcon } from "@heroicons/react/24/outline";
+"use client";
+import { BellIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import {
   Bars3BottomLeftIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { useState } from "react";
+import Notification from "../../components/notification";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const DocumentsLayout = ({ children }: Props) => {
+  const [openNotification, setOpenNotification] = useState(false);
+
   const links = {
     user: [
       {
@@ -28,9 +33,9 @@ const DocumentsLayout = ({ children }: Props) => {
   };
 
   return (
-    <div className="grid h-screen grid-rows-[auto,1fr] gap-4  tablet:gap-10">
+    <div className="relative grid h-screen grid-rows-[auto,1fr] gap-4  tablet:gap-10">
       <nav>
-        <div className="navbar bg-base-100 py-4">
+        <div className="navbar bg-base-100 py-4 px-5">
           <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn-ghost btn-circle btn">
@@ -60,18 +65,37 @@ const DocumentsLayout = ({ children }: Props) => {
                 className="object-cover"
               />
               <h1 className="whitespace-nowrap">
-                Barangay <span className="font-bold whitespace-nowrap">Victoria Reyes</span>
+                Barangay{" "}
+                <span className="whitespace-nowrap font-bold">
+                  Victoria Reyes
+                </span>
               </h1>
             </div>
           </div>
-          <div className="navbar-end">
-            <button className="btn-ghost btn-circle btn">
-              <MagnifyingGlassIcon className="h-5 w-5" />
-            </button>
-            <button className="btn-ghost btn-circle btn">
-              <BellIcon className="h-5 w-5" />
-            </button>
+          <div className="navbar-end flex cursor-pointer items-center gap-6">
+            <div className="flex items-center gap-2 group">
+              <BellIcon
+                onClick={() => setOpenNotification(!openNotification)}
+                className="h-5 w-5 group-hover:text-brand"
+              />
+              <span
+                onClick={() => setOpenNotification(!openNotification)}
+                className="text-sm hover:text-brand">
+                Notifications
+              </span>
+            </div>
+
+            <div className="flex items-center gap-2 group">
+              <UserCircleIcon className="w-5 h-5 group-hover:text-brand"/>
+              <span className="text-sm hover:text-brand">Logout my Account</span>
+            </div>
           </div>
+
+          {openNotification && (
+            <Notification
+              handler={() => setOpenNotification(!openNotification)}
+            />
+          )}
         </div>
       </nav>
 
