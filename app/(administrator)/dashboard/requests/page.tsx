4bdfa424778e98a88d240c4006fc6e4eecdccf52
jazztine <jzztn.crz@gmail.com>
2022-11-17 21:45:51 +0800

    Add admin requests page

--- app/(administrator)/dashboard/requests/page.tsx
+++ app/(administrator)/dashboard/requests/page.tsx
@@ -1,5 +1,82 @@
-const DashboardRequestsPage = () => {
-  return <div>RequestPage</div>;
+import { use } from "react";
+import moment from "moment";
+import database from "../../../../library/database";
+import Table from "../../../../components/dashboard/sections/table";
+
+const getUsers = async () => {
+  const users = await database.user.findMany({
+    where: {
+      verified: true,
+    },
+    include: {
+      address: true,
+      households: true,
+    },
+  });
+  return users;
 };
 
-export default DashboardRequestsPage;
+const DashboardRequestPage = () => {
+  const users = use(getUsers());
+
+  const fields = [
+    "Resident ID",
+    "Document Name",
+    "Request Date",
+    "Document Status",
+    "First Name",
+    "Middle Name",
+    "Last Name",
+    "Email Address",
+    "Gender",
+    "Birth Date",
+    "Address",
+    "Birth Place",
+    "Phone Number",
+    "Occupation",
+    "Households",
+    "Homeowner",
+    "Voter",
+    "Account Verified",
+  ];
+
+  return (
+    <div className="space-section">
+      <Table.Main name="List of ">
+        <Table.Head>
+          <Table.Row heading>
+            {fields.map((field) => (
+              <Table.Heading key={field}>{field}</Table.Heading>
+            ))}
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {users.map((user) => (
+            <Table.Row key={user.id}>
+              <Table.Data value={user.id} />
+              <Table.Data value="Barangay Indigency" />
+              <Table.Data value="September 22, 2022" />
+              <Table.Data value="PENDING" />
+              <Table.Data value={user.givenName} />
+              <Table.Data value={user.middleName} />
+              <Table.Data value={user.familyName} />
+              <Table.Data value={user.email} />
+              <Table.Data value={user.gender} />
+              <Table.Data value={moment(user.birthdate).format("LL")} />
+              <Table.Data value={user.address!.street} />
+              <Table.Data value={user.birthplace} />
+              <Table.Data value={user.phone} />
+              <Table.Data value={user.occupation} />
+              <Table.Data value={user.households.length} />
+              <Table.Data value={user.homeowner} />
+              <Table.Data value={user.voter} />
+              <Table.Data value={user.verified} />
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Main>
+    </div>
+  );
+};
+
+export default DashboardRequestPage;
