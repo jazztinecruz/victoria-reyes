674c217c134633dc6eda75e1f454d47de2a466fe
jazztine <jzztn.crz@gmail.com>
2022-12-05 23:11:29 +0800

    Add ISR

--- app/(administrator)/dashboard/layout.tsx
+++ app/(administrator)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import Breadcrumbs from "./breadcrumbs";
 
+export const revalidate = 10;
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="notebook:space-section space-content smooth">
