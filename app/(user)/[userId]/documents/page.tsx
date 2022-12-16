f69cd95d4330d0c429afbeec2baa0fca59aa2bbe
jazztine <jzztn.crz@gmail.com>
2022-12-16 13:20:32 +0800

    Add confirmation modal

--- app/(user)/[userId]/documents/page.tsx
+++ app/(user)/[userId]/documents/page.tsx
@@ -5,7 +5,8 @@ import Button from "../../../../components/elements/button/button";
 import Modal from "../../../../components/elements/modal";
 
 const Documents = () => {
-  const [openRequestModal, setopenRequestModal] = useState(false);
+  const [openRequestModal, setOpenRequestModal] = useState(false);
+  const [openConfirmationModal, setOpenConfirmationModal] = useState(false);
 
   const documents = [
     {
@@ -34,14 +35,14 @@ const Documents = () => {
     <div className="grid h-full place-items-center">
       <div className="grid grid-flow-row items-start justify-start gap-6 tablet:grid-cols-2">
         {documents.map((document) => (
-          <div className="card w-96 cursor-pointer bg-white border-2 border-gray-100 text-center">
+          <div className="card w-96 cursor-pointer border-2 border-gray-100 bg-white text-center">
             <div className="card-body gap-6">
               <h2 className="card-title justify-center">{document.name}</h2>
               <p>{document.description}</p>
               <div className="card-actions justify-center">
                 <button
-                  onClick={() => setopenRequestModal(!openRequestModal)}
-                  className="btn-primary btn bg-brand hover:btn-ghost border-none">
+                  onClick={() => setOpenRequestModal(!openRequestModal)}
+                  className="btn-primary btn border-none bg-brand hover:btn-ghost">
                   Request
                 </button>
               </div>
@@ -54,13 +55,46 @@ const Documents = () => {
             size="medium"
             as="div"
             open
-            onClose={() => setopenRequestModal(!openRequestModal)}>
+            onClose={() => setOpenRequestModal(!openRequestModal)}>
+            <div className="flex flex-col items-center justify-center gap-5 text-center">
+              <span className="mt-5 text-xl font-semibold text-brand">
+                Are you sure you want to request this document?
+              </span>
+              <div className="mt-6 flex items-center gap-4">
+                <Button
+                  handler={() => setOpenRequestModal(!openRequestModal)}
+                  name="Go Back"
+                />
+                <Button
+                  handler={() =>
+                    setOpenConfirmationModal(!openConfirmationModal)
+                  }
+                  fill
+                  name="Continue"
+                />
+              </div>
+            </div>
+          </Modal>
+        )}
+
+        {openConfirmationModal && (
+          <Modal
+            size="medium"
+            as="div"
+            open
+            onClose={() => setOpenConfirmationModal(!openConfirmationModal)}>
             <div className="flex flex-col items-center justify-center gap-5 text-center">
               <span className="mt-5 text-xl font-semibold text-brand">
                 You've succesfully requested!
               </span>
               <span className="text-gray mb-4 text-sm">
-                <span className="font-sembiold">Note:</span> You can only
+                <span>
+                  Please prepare <span className="font-semibold">50.00</span>{" "}
+                  upon claiming.
+                </span>
+                <br></br>
+                <br></br>
+                <span className="font-semibold">Note:</span> You can only
                 request a document from{" "}
                 <span className="font-semibold">8:00 am</span> to{" "}
                 <span className="font-semibold">11:00 am</span>. All documents
@@ -73,7 +107,14 @@ const Documents = () => {
                   and claim your document to our office.
                 </span>
               </span>
-              <Button handler={() => setopenRequestModal(!openRequestModal)} name="Got it!" />
+              <Button
+                handler={() => {
+                  setOpenConfirmationModal(!openConfirmationModal);
+                  setOpenRequestModal(false);
+                }}
+                name="I Got it!"
+                fill
+              />
             </div>
           </Modal>
         )}
