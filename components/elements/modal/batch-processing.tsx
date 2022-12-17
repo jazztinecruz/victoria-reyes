a730169e6e50d984b45b854b3a8755d6e3231309
jazztine <jzztn.crz@gmail.com>
2022-12-17 15:09:29 +0800

    Add batch processing modal

---
+++ components/elements/modal/batch-processing.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useState } from "react";
+import Modal from ".";
+import Button from "../button/button";
+
+const BatchProcessingModal = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <div>
+      {/* button */}
+      <div className="mr-auto">
+        <Button
+          name="Batch Process"
+          fill
+          handler={() => setOpenModal(!openModal)}
+        />
+      </div>
+
+      {openModal && (
+        <Modal
+          size="medium"
+          as="div"
+          open
+          onClose={() => setOpenModal(!openModal)}>
+          <div className="flex flex-col items-center justify-center gap-3 text-center">
+            <span className="mt-5 text-lg font-medium">
+              Are you sure you want to do batch processing?
+            </span>
+            <span className="-mt-2 text-sm font-medium opacity-50">
+              This will print all documents in one go
+            </span>
+            <div className="mt-4 flex items-center gap-4">
+              <Button handler={() => setOpenModal(!openModal)} name="Go Back" />
+              <Button
+                handler={() => setOpenModal(!openModal)}
+                fill
+                name="Continue"
+              />
+            </div>
+          </div>
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export default BatchProcessingModal;
