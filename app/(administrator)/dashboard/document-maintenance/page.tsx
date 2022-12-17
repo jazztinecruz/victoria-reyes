1e3dcd0da56cfd07d13a388113c01788522cd4c6
jazztine <jzztn.crz@gmail.com>
2022-12-17 19:13:15 +0800

    Modify document maintenance

--- app/(administrator)/dashboard/document-maintenance/page.tsx
+++ app/(administrator)/dashboard/document-maintenance/page.tsx
@@ -3,8 +3,7 @@ import { useState } from "react";
 import DeleteDocument from "../../../../components/documents/delete-document";
 import EditDocument from "../../../../components/documents/edit-document";
 import Button from "../../../../components/elements/button/button";
-import Field from "../../../../components/elements/field";
-import Modal from "../../../../components/elements/modal";
+import AddDocumentModal from "../../../../components/elements/modal/add-document";
 import Document from "../../../document";
 
 const DocumentMaintenance = () => {
@@ -77,55 +76,15 @@ const DocumentMaintenance = () => {
       </div>
 
       {openAddModal && (
-        <Modal
-          size="medium"
-          as="div"
-          open
-          onClose={() => setOpenAddModal(!openAddModal)}>
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold">Additional Document</span>
-            <span className="text-sm font-light opacity-50">
-              Provide necessary details to add document
-            </span>
-          </div>
-          <form
-            onSubmit={() => {
-              setDocuments([...documents, document]);
-              setDocument(initialValue);
-              setOpenAddModal(false);
-            }}
-            className="flex flex-col gap-10">
-            <Field.Textbox
-              type="text"
-              name="name"
-              label="Name of Document"
-              required
-              onChange={setDocument}
-            />
-            <div className="flex flex-col gap-3">
-              <div className="flex flex-col">
-                <span className="text-lg font-semibold">
-                  Description of Document
-                </span>
-                <span className="text-sm font-light opacity-50">
-                  You can stretch out this field for a better view
-                </span>
-              </div>
-              <textarea
-                name="description"
-                required
-                onChange={(event) =>
-                  setDocument({
-                    ...document,
-                    description: event?.target.value,
-                  })
-                }
-                className="mt-2 block w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 placeholder-gray-400 focus:border-brand/75 focus:outline-none focus:ring focus:ring-brand/75 focus:ring-opacity-40  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:placeholder-gray-600"
-              />
-            </div>
-            <Button fill name="Add Document" />
-          </form>
-        </Modal>
+        <AddDocumentModal
+          setDocument={setDocument}
+          document={document}
+          documents={documents}
+          setDocuments={setDocuments}
+          openAddModal={openAddModal}
+          setOpenAddModal={setOpenAddModal}
+          initialValue={initialValue}
+        />
       )}
     </div>
   );
