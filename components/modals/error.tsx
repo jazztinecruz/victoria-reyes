33fc189e2281ce6249896314928711cb1dad2ad6
jazztine <jzztn.crz@gmail.com>
2023-04-09 11:34:53 +0800

    chore(modals): modify static messages of error modal

--- components/modals/error.tsx
+++ components/modals/error.tsx
@@ -12,9 +12,9 @@ const ErrorModal = ({ onClose, handler, errorMessage }: Props) => {
     <Modal size="medium" as="div" open onClose={onClose}>
       <div className="flex flex-col items-center justify-center gap-5 text-center">
         <span className="mt-5 text-xl font-semibold text-brand">
-          Oops.. {errorMessage} Please Try Again
+          {errorMessage}
         </span>
-        <Button handler={handler} fill name="Go Back" />
+        <Button handler={handler} fill name="Try Aagin" />
       </div>
     </Modal>
   );
