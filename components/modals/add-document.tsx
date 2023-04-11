d740ece56874a7929c2cef7af787385f8d0fe145
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-11 22:29:14 +0800

    Modify add modal

--- components/modals/add-document.tsx
+++ components/modals/add-document.tsx
@@ -1,103 +1,90 @@
-import { Dispatch, SetStateAction } from "react";
+"use client";
+import { useState } from "react";
 import Modal from ".";
 import Button from "../elements/button/button";
 import Field from "../elements/field";
 
-interface Props {
-  openAddModal: boolean;
-  setOpenAddModal: Dispatch<SetStateAction<boolean>>;
-  document: {
-    id: number;
-    name: string;
-    description: string;
-  };
-  setDocument: Dispatch<
-    SetStateAction<{
-      id: number;
-      name: string;
-      description: string;
-    }>
-  >;
-  documents: {
-    id: number;
-    name: string;
-    description: string;
-  }[];
-  setDocuments: Dispatch<
-    SetStateAction<
-      {
-        id: number;
-        name: string;
-        description: string;
-      }[]
-    >
-  >;
-  initialValue: {
-    id: number;
-    name: string;
-    description: string;
-  };
-}
+const AddDocumentModal = () => {
+  const [openAddModal, setOpenAddModal] = useState(false);
+  const [fields, setFields] = useState({
+    title: "",
+    description: "",
+    price: "",
+  });
 
-const AddDocumentModal = ({
-  openAddModal,
-  setOpenAddModal,
-  documents,
-  setDocuments,
-  document,
-  setDocument,
-  initialValue,
-}: Props) => {
+  const handleAddDocument = async () => {
+    const response = await fetch("/api/add-document", {
+      method: "POST",
+      body: JSON.stringify({
+        title: fields.title,
+        description: fields.description,
+        price: +fields.price,
+      }),
+    });
+  };
+
   return (
-    <Modal
-      size="medium"
-      as="div"
-      open
-      onClose={() => setOpenAddModal(!openAddModal)}>
-      <div className="flex flex-col">
-        <span className="text-lg font-semibold">Additional Document</span>
-        <span className="text-sm font-light opacity-50">
-          Provide necessary details to add document
-        </span>
-      </div>
-      <form
-        onSubmit={() => {
-          setDocuments([...documents, document]);
-          setDocument(initialValue);
-          setOpenAddModal(false);
-        }}
-        className="flex flex-col gap-10">
-        <Field.Textbox
-          type="text"
-          name="name"
-          label="Name of Document"
-          required
-          onChange={setDocument}
+    <>
+      <div className="mr-auto">
+        <Button
+          name="Add Document"
+          fill
+          handler={() => setOpenAddModal(true)}
         />
-        <div className="flex flex-col gap-3">
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold">
-              Description of Document
-            </span>
-            <span className="text-sm font-light opacity-50">
-              You can stretch out this field for a better view
-            </span>
-          </div>
-          <textarea
-            name="description"
-            required
-            onChange={(event) =>
-              setDocument({
-                ...document,
-                description: event?.target.value,
-              })
-            }
-            className="mt-2 block w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 placeholder-gray-400 focus:border-brand/75 focus:outline-none focus:ring focus:ring-brand/75 focus:ring-opacity-40  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:placeholder-gray-600"
-          />
+      </div>
+
+      <Modal
+        size="medium"
+        as="div"
+        open={openAddModal}
+        onClose={() => setOpenAddModal(false)}>
+        <div className="flex flex-col">
+          <span className="text-lg font-semibold">Additional Document</span>
+          <span className="text-sm font-light opacity-50">
+            Provide necessary details to add document
+          </span>
         </div>
-        <Button fill name="Add Document" />
-      </form>
-    </Modal>
+        <form className="flex flex-col gap-10">
+          <Field.Textbox
+            type="text"
+            name="title"
+            label="Name of Document"
+            required
+            value={fields.title}
+            onChange={setFields}
+          />
+          <Field.Textbox
+            type="number"
+            name="price"
+            value={fields.price}
+            label="Price of Document"
+            required
+            onChange={setFields}
+          />
+
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-col">
+              <span className="text-lg font-semibold">
+                Description of Document
+              </span>
+              <span className="text-sm font-light opacity-50">
+                You can stretch out this field for a better view
+              </span>
+            </div>
+            <textarea
+              name="description"
+              required
+              value={fields.description}
+              onChange={(event) =>
+                setFields({ ...fields, description: event.target.value })
+              }
+              className="mt-2 block w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 placeholder-gray-400 focus:border-brand/75 focus:outline-none focus:ring focus:ring-brand/75 focus:ring-opacity-40  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:placeholder-gray-600"
+            />
+          </div>
+          <Button fill name="Add Document" handler={handleAddDocument} />
+        </form>
+      </Modal>
+    </>
   );
 };
 
