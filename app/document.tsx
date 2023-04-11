6b387c4d8b5095c2c0bc6f769ddeca004e4d36a3
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-11 23:03:36 +0800

    Modify document UI

--- app/document.tsx
+++ app/document.tsx
@@ -1,17 +1,17 @@
 type Props = {
   title: string;
   description: string;
-  price: number
+  price: number;
 };
 
 const Document = ({ title, description, price }: Props) => {
   return (
     <div className="group flex cursor-pointer flex-col items-center gap-4 px-6 py-4 text-center">
-      <div>{price}</div>
       <span className="text-lg font-semibold group-hover:text-brand laptop:text-xl">
         {title}
       </span>
       <p className="leading-relaxed  group-hover:text-brand">{description}</p>
+      <span className="font-semibold">Amount: {price}</span>
     </div>
   );
 };
