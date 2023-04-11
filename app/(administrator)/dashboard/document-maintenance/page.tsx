7256670370e70cd1e594274031cc47c0f40e3638
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-11 23:03:25 +0800

    Modify document maintenance page

--- app/(administrator)/dashboard/document-maintenance/page.tsx
+++ app/(administrator)/dashboard/document-maintenance/page.tsx
@@ -1,91 +1,42 @@
-"use client";
-import { useState } from "react";
-import DeleteDocument from "../../../../components/documents/delete-document";
-import EditDocument from "../../../../components/documents/edit-document";
-import Button from "../../../../components/elements/button/button";
+import { use } from "react";
+import EditDocument from "../../../../components/modals/edit-document";
 import AddDocumentModal from "../../../../components/modals/add-document";
+import database from "../../../../library/database";
 import Document from "../../../document";
+import DeleteDocument from "../../../../components/modals/delete-document";
+
+const getDocuments = async () => {
+  const documents = await database.document.findMany();
+  return documents;
+};
 
 const DocumentMaintenance = () => {
-  const [openAddModal, setOpenAddModal] = useState(false);
-  const initialValue = {
-    id: 0,
-    name: "",
-    description: "",
-  };
-  const [document, setDocument] = useState(initialValue);
-  const [documents, setDocuments] = useState([
-    {
-      id: 1,
-      name: "Barangay Clearance",
-      description:
-        "A Barangay Clearance one of the easiest documents you can get as a valid proof of your identity. It is a document that contains a person's name, address, thumb mark, and signature. It also contains the date it was issued and for what specific purpose.",
-    },
-    {
-      id: 2,
-      name: "Barangay Indigency",
-      description:
-        "A document that are sometimes required by the Philippine government or a private institution as proof of an individual's financial situation.",
-    },
-    {
-      id: 3,
-      name: "Barangay ID",
-      description:
-        "A Barangay Clearance one of the easiest documents you can get as a valid proof of your identity. It is a document that contains a person's name, address, thumb mark, and signature. It also contains the date it was issued and for what specific purpose.",
-    },
-    {
-      id: 4,
-      name: "Cedula",
-      description:
-        "one of the basic requirements for most government transactions. It can also serve as valid identification for individuals and corporations residing or located in the same municipality from where it's acquired",
-    },
-  ]);
+  const documents = use(getDocuments());
 
   return (
     <div className="grid gap-8">
-      {/* button */}
-      <div className="mr-auto">
-        <Button
-          name="Add Document"
-          fill
-          handler={() => setOpenAddModal(!openAddModal)}
-        />
-      </div>
+      {/* button and modal */}
+      <AddDocumentModal />
 
       {/* documents */}
       <div className="grid grid-cols-1 gap-6 rounded-md tablet:grid-cols-2">
-        {documents.map((document, index) => (
+        {documents.map((document) => (
           <div
-            key={index}
+            key={document.id}
             className="grid grid-rows-[auto,1fr] rounded-md border-2 border-gray-200 p-6">
             <div className="ml-auto flex items-center gap-4">
-              <EditDocument
-                document={document}
-                documents={documents}
-                setDocuments={setDocuments}
-              />
-              <DeleteDocument
-                document={document}
-                documents={documents}
-                setDocuments={setDocuments}
-              />
+              <EditDocument document={document} />
+              <DeleteDocument document={document} />
             </div>
-            <Document name={document.name} description={document.description} />
+            <Document
+              key={document.id}
+              title={document.title}
+              description={document.description}
+              price={document.price}
+            />
           </div>
         ))}
       </div>
-
-      {openAddModal && (
-        <AddDocumentModal
-          setDocument={setDocument}
-          document={document}
-          documents={documents}
-          setDocuments={setDocuments}
-          openAddModal={openAddModal}
-          setOpenAddModal={setOpenAddModal}
-          initialValue={initialValue}
-        />
-      )}
     </div>
   );
 };
