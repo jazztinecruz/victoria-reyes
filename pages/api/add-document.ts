6d279efa673c45e1194c762ef8aac13a21cebf3f
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-11 22:29:29 +0800

    Add api for add document

---
+++ pages/api/add-document.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import database from "../../library/database";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method !== "POST") {
+    return response.status(405).send({ message: "Invalid Method" });
+  }
+  const body = JSON.parse(request.body);
+  try {
+    const document = await database.document.create({
+      data: {
+        title: body.title,
+        description: body.description,
+        price: body.price,
+      },
+    });
+    return response.status(201).send(document);
+  } catch (error) {
+    return response.status(505).send({ message: "Internal server error" });
+  }
+};
+
+export default handler;
