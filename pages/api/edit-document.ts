022e2d76e894ac7f25a2f52e0f2d0fac40362a49
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-11 22:56:48 +0800

    Add api for edit document

---
+++ pages/api/edit-document.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import database from "../../library/database";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method !== "PUT") {
+    return response.status(405).send({ message: "Invalid Method" });
+  }
+  const body = JSON.parse(request.body);
+  try {
+    const document = await database.document.update({
+      where: {
+        id: body.id,
+      },
+      data: {
+        title: body.title,
+        description: body.description,
+        price: body.price,
+      },
+    });
+    return response.status(201).send(document);
+  } catch (error) {
+    return response.status(505).send({ message: "Internal server error" });
+  }
+};
+
+export default handler;
