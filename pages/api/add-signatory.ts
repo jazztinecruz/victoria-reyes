45cecd71fa3bf3245f447bb47e9804410c3776e7
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-13 00:36:45 +0800

    Add api for adding signatory

---
+++ pages/api/add-signatory.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import database from "../../library/database";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method !== "POST") {
+    return response.status(405).send({ message: "Invalid Method" });
+  }
+  const body = JSON.parse(request.body);
+  try {
+    const document = await database.signatory.create({
+      data: {
+        firstName: body.firstName,
+        middleName: body.middleName,
+        lastName: body.lastName,
+      },
+    });
+    return response.status(201).send(document);
+  } catch (error) {
+    return response.status(505).send({ message: "Internal server error" });
+  }
+};
+
+export default handler;
