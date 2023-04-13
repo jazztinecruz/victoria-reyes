e66717890555cd2b681fdbe03688aa634448f769
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-13 20:46:40 +0800

    Modify payment maintenance

--- app/(administrator)/dashboard/payment-maintenance/page.tsx
+++ app/(administrator)/dashboard/payment-maintenance/page.tsx
@@ -1,70 +1,69 @@
-"use client";
-
-import { useState } from "react";
-import Table from "../../../../components/table";
-import Button from "../../../../components/elements/button/button";
+import { use } from "react";
 import AddTransaction from "../../../../components/modals/add-transaction";
+import Table from "../../../../components/table";
+import database from "../../../../library/database";
+
+const getApprovedDocuments = async () => {
+  const approvedRequests = await database.request.findMany({
+    where: {
+      status: "APPROVED",
+    },
+    include: {
+      user: true,
+      document: true,
+    },
+  });
+  return approvedRequests;
+};
 
 const PaymentMaintenance = () => {
-  const [openModal, setOpenModal] = useState(false);
-  const initialValue = {
-    type: "",
-    amount: 0,
-    processor: "",
-    date: Date.now(),
-  };
-  const [fields, setFields] = useState(initialValue);
-  const [transactions, setTransactions] = useState<typeof fields[]>([]);
-  const headings = ["No", "Document Type", "Amount", "Process By", "Date"];
+  const approvedRequests = use(getApprovedDocuments());
+
+  const totalPayment = approvedRequests.reduce((prevPrice, transaction) => {
+    return prevPrice + transaction?.document!.price;
+  }, 0);
+
+  const headers = [
+    "No.",
+    "Document ID",
+    "Document Title",
+    "Document Price",
+    "Document Status",
+    "First Name",
+    "Middle Name",
+    "Last Name",
+    "Resident ID",
+  ];
 
   return (
     <div className="grid gap-10">
-      {/* button */}
-      <div className="mr-auto">
-        <Button
-          name="Add Payment"
-          fill
-          handler={() => setOpenModal(!openModal)}
-        />
-      </div>
+      <AddTransaction />
       {/* table */}
-      <div className="w-full">
-        <Table.Main name="Transaction History">
-          <Table.Head>
-            <Table.Row heading>
-              {headings.map((head) => (
-                <Table.Heading key={head}>{head}</Table.Heading>
-              ))}
-            </Table.Row>
-          </Table.Head>
-          <Table.Body>
-            {transactions.map((transaction, index) => (
-              <Table.Row key={index}>
-                <Table.Data value={index + 1} />
-                <Table.Data value={transaction.type} />
-                <Table.Data value={transaction.amount} />
-                <Table.Data value={transaction.processor} />
-                <Table.Data value={transaction.date} />
-              </Table.Row>
+      <span className="font-semibold">Total Payment: {totalPayment}</span>
+      <Table.Main name="Transaction Book">
+        <Table.Head>
+          <Table.Row heading>
+            {headers.map((header) => (
+              <Table.Heading key={header}>{header}</Table.Heading>
             ))}
-          </Table.Body>
-        </Table.Main>
-      </div>
-
-      <span className="font-semibold">Total Payment: 1000.00</span>
-
-      {/* modal */}
-      {openModal && (
-        <AddTransaction
-          openModal={openModal}
-          setOpenModal={setOpenModal}
-          fields={fields}
-          setFields={setFields}
-          transactions={transactions}
-          setTransactions={setTransactions}
-          initialValue={initialValue}
-        />
-      )}
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {approvedRequests.map((transaction, index) => (
+            <Table.Row key={transaction.id}>
+              <Table.Data value={index + 1} />
+              <Table.Data value={transaction.documentId} />
+              <Table.Data value={transaction.document?.title} />
+              <Table.Data value={transaction.document?.price} />
+              <Table.Data value={transaction.status} />
+              <Table.Data value={transaction.user?.givenName} />
+              <Table.Data value={transaction.user?.middleName} />
+              <Table.Data value={transaction.user?.familyName} />
+              <Table.Data value={transaction.userId} />
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Main>
     </div>
   );
 };
