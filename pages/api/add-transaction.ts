cf91aadb49e49f819b67e1087d1b484812c4fe58
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-13 20:46:30 +0800

    Add api for add transaction

---
+++ pages/api/add-transaction.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import database from "../../library/database";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method !== "POST") {
+    return response.status(405).send({ message: "Invalid Method" });
+  }
+  const body = JSON.parse(request.body);
+  try {
+    const document = await database.request.create({
+      data: {
+        status: "APPROVED",
+        documentId: body.documentId,
+        userId: body.userId,
+      },
+    });
+    return response.status(201).send(document);
+  } catch (error) {
+    return response.status(505).send({ message: "Internal server error" });
+  }
+};
+
+export default handler;
