35d433075ef1c9c715794ed0962e69ad50320f99
Jazztine Cruz <jzztn.crz@gmail.com>
2023-04-19 11:02:30 +0800

    Delete confirmed request modal

--- components/modals/user-request-document.tsx
+++ components/modals/user-request-document.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from "react";
-import ConfirmedDocumentrequest from "./confirmed-request";
 import SuccessfulModal from "../modals/sucessful";
 import Button from "../elements/button/button";
+import Modal from ".";
 
 interface Props {
   userId: string;
@@ -38,14 +38,38 @@ const UserRequestDocumentButton = ({ userId, documentId, price }: Props) => {
       </button>
 
       {openConfirmationModal && (
-        <ConfirmedDocumentrequest
-          price={price}
-          onClose={() => setOpenConfirmationModal(!openConfirmationModal)}
-          handler={() => {
-            handleRequestDocument();
-            setOpenConfirmationModal(false);
-          }}
-        />
+        <Modal
+          size="medium"
+          as="div"
+          open
+          onClose={() => setOpenConfirmationModal(false)}>
+          <div className="flex flex-col items-center justify-center gap-5 text-center">
+            <span className="mt-5 text-xl font-semibold text-brand">
+              Are you sure you want to request this document?
+            </span>
+            <span className="text-gray mb-4 text-sm">
+              <span>
+                Please prepare <span className="font-semibold">{price}.00</span>{" "}
+                upon claiming.
+              </span>
+              <br></br>
+              <br></br>
+              <span className="font-semibold">Note:</span> You can only request
+              a document from <span className="font-semibold">8:00 am</span> to
+              <span className="font-semibold">11:00 am</span>. All documents
+              that are requested after the desginated time will be process
+              tomorrow.
+            </span>
+            <Button
+              handler={() => {
+                handleRequestDocument();
+                setOpenConfirmationModal(false);
+              }}
+              name="Request"
+              fill
+            />
+          </div>
+        </Modal>
       )}
 
       {openSuccessfulModal ? (
